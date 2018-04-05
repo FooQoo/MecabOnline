3ab1ad45af0cd99d90c084db7b8ac83cb4da219a
MtHappy <horseshoes.buzz.17@gmail.com>
2018-04-05 10:56:55 +0900

    del fun.py

--- func.py
+++
@@ -1,8 +0,0 @@
-import MeCab
-
-def func(txt):
-    segments = []
-    mecab = MeCab.Tagger('-d /usr/lib/mecab/dic/mecab-ipadic-neologd')
-    morphs = [tuple(m.split('\t')) for m in mecab.parse(txt).split('\n')]
-    segments += [' : '.join(m) for m in morphs if len(m) == 2]
-    return segments
